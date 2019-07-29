ac1dd9191a40629eb5fc7083ac4b13fadac07a9e
likgoek <jonathanstroem91@gmail.com>
2019-07-29 10:02:16 -0400

    BC analysis start

--- code/chapter3/bc_entry_rates.py
+++ code/chapter3/bc_entry_rates.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 def j_ck(u, c):
     L = 15e-2 # slab thickness
@@ -11,30 +12,24 @@ def j_ck(u, c):
     return j
 
 
-#df = pd.read_csv('../../data/chapter3/bc_entry_rates.csv', header=4)
+df = pd.read_csv('../../data/chapter3/bc_entry_rates.csv', header=4)
+new_names = (
+    'soil',
+    'p',
+    'p_not_used',
+    'j_ghost',
+    'c',
+    'u',
+)
 
-# TODO: Read u and c from the dataframe and use that to calculate j_ck, then compare with n from COMSOL
-# p, u, c = df['p'], df['u'], df['c']
+df.rename(columns=dict(zip(list(df), new_names)), inplace=True)
+df['j_expr'] = j_ck(df['u'], df['c'])
 
-c = 0.01
-u = np.logspace(-12,-2)
+fig, (ax1, ax2) = plt.subplots(2,1,dpi=300,sharex=True)
 
+sns.lineplot(data=df, x='p', y='j_ghost', hue='soil', ax=ax1)
+sns.lineplot(data=df, x='p', y='j_expr', hue='soil', ax=ax2)
 
-fig, (ax1, ax2) = plt.subplots()
-
-# change u to p instead
-ax1.loglog(u,j_ck(u,c), label='Flow into house')
-ax1.loglog(u,j_ck(-u,c), label='Flow out of house')
-
-ax1.set_title('Analytic flux expression')
-ax1.set_ylim(bottom=1e-10)
-
-fig, ax = plt.subplots(dpi=300)
-
-ax.semilogy(p, j_ck(u, c), label='Flux expression')
-ax.semilogy(p, n, label='Ghost cell flux')
-
-ax2.set_title('Flux into ghost cell')
-
-ax1.legend()
+ax1.set_yscale('log')
+ax2.set_yscale('log')
 plt.show()
