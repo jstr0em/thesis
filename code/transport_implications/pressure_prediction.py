09dfeea7e5e2567694824bd8af91809f9d30b030
likgoek <jonathanstroem91@gmail.com>
2020-03-18 14:45:42 -0400

    First draft

--- code/transport_implications/pressure_prediction.py
+++ code/transport_implications/pressure_prediction.py
@@ -6,20 +6,23 @@ from matplotlib.gridspec import GridSpec
 from matplotlib.figure import figaspect
 
 plt.style.use('seaborn')
-def dP_T(dT):
 
-    alpha = 0.040 # Pa/(m*K)
-    dz = -3 # basement floor depth from surface m
-    return alpha*dz*dT
+def get_density_of_air(T,p):
+    R_spec = 287.058 # specific gas constant for dry air, i.e. R/M where M is the molar mass
+    return p/(R_spec*T)
+
+def dP_T(Ti, To, p_bar=101325, dz=-3):
+    #rho = 1
+    R_spec = 287.058 # specific gas constant for dry air, i.e. R/M where M is the molar mass
+    rho = get_density_of_air(To, p_bar)
+
+    g = 9.82
+    return rho*g*((Ti-To)/Ti)*dz
 
 
-def dP_T2(Ti, To, dz):
-    alpha = 3454 # Pa*K/m
-    return alpha*(1/Ti-1/To)*dz
-
-def dP_wind(u, dir=None):
-    Cj = 0.3 # surface pressure coefficient
-    rho = 1 # air density kg/m^3
+def dP_wind(u, dir=None, Cp=1, T=298, p_bar=101325):
+    #rho = 1 # air density kg/m^3
+    rho = get_density_of_air(T, p_bar)
     sign = {
         'N': 1,
         'NE': 1,
@@ -33,13 +36,13 @@ def dP_wind(u, dir=None):
 
 
     if dir is None:
-        return 0.5*Cj*rho*u**2
+        return 0.5*Cp*rho*u**2
     else:
         signs = []
         for dir_now in dir:
             signs.append(sign[dir_now])
         signs = np.array(signs)
-        return 0.5*Cj*rho*-signs*u**2
+        return 0.5*Cp*rho*-signs*u**2
 
 
 
@@ -68,20 +71,19 @@ def get_wind_direction(degree):
 
 df['Cardinal'] = df['WindDir'].apply(get_wind_direction)
 
-df['dP_wind'] = dP_wind(df['WindSpeed'])
-
-df['dP_wind_corr'] = dP_wind(df['WindSpeed'], df['Cardinal'].values)
-
 df['dT'] = df['IndoorTemp'] - df['OutdoorTemp']
 
 df['Ti'] = df['IndoorTemp']+273.15
 df['To'] = df['OutdoorTemp']+273.15
 
 
-df['dP_T'] = dP_T2(df['Ti'], df['To'], 3)
+df['dP_T'] = dP_T(df['Ti'], df['To'], dz=-3)
+
+df['dP_wind'] = dP_wind(df['WindSpeed'], df['Cardinal'].values, Cp=0.35, T=df['To'], p_bar=df['BarometricPressure'])
+
+
 
 df['dP_T_wind'] = df['dP_T'] + df['dP_wind']
-df['dP_T_wind_corr'] = df['dP_T'] + df['dP_wind_corr']
 df['Time'] = df['Time'].apply(pd.to_datetime)
 
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
@@ -94,11 +96,10 @@ gs = GridSpec(3,6, figure=fig)
 ax1 = fig.add_subplot(gs[0,0:3])
 ax2 = fig.add_subplot(gs[0,3:],sharex=ax1)
 # bottom row
-ax6 = fig.add_subplot(gs[2:,:],sharex=ax1)
+ax5 = fig.add_subplot(gs[2:,:],sharex=ax1)
 # middle row
-ax3 = fig.add_subplot(gs[1,0:2],sharex=ax1)
-ax4 = fig.add_subplot(gs[1,2:4],sharex=ax1)
-ax5 = fig.add_subplot(gs[1,4:],sharex=ax1)
+ax3 = fig.add_subplot(gs[1,0:3],sharex=ax1)
+ax4 = fig.add_subplot(gs[1,3:],sharex=ax1)
 
 fig.subplots_adjust(top=0.85)
 
@@ -114,7 +115,6 @@ ax2.tick_params(axis='x', labelbottom=False)
 # middle row plots (individual dP contributions)
 df.plot( x='Time', y='dP_T', ax=ax3, color=colors[1], legend=False)
 df.plot( x='Time', y='dP_wind', ax=ax4, color=colors[1], legend=False)
-df.plot( x='Time', y='dP_wind_corr', ax=ax5, color=colors[1], legend=False)
 
 # cosmetic stuff
 
@@ -131,26 +131,20 @@ ax4.set(
     #ylim=pressure_limits,
 )
 
-ax5.set(
-    xlabel='',
-    title='Direction corrected wind contr.',
-    #ylim=pressure_limits,
-)
  # N, NE, E, SE => negative
 ax4.tick_params(axis='x', labelbottom=False)
-ax5.tick_params(axis='x', labelbottom=False)
 
 # comparing the actual and predicted values
-df.plot( x='Time', y='IndoorOutdoorPressure', ax=ax6, )
-df.plot( x='Time', y='dP_T_wind_corr', ax=ax6, alpha=0.8, )
+df.plot( x='Time', y='IndoorOutdoorPressure', ax=ax5, )
+df.plot( x='Time', y='dP_T_wind', ax=ax5, alpha=0.8, )
 
 # cosmetic stuff
-ax6.set(
+ax5.set(
     ylabel='$\\Delta p$ (Pa)',
-    title='Temperature + (corrected) wind contributions vs. recorded pressure difference',
+    title='Temperature + wind contributions vs. recorded pressure difference',
     xlabel='Time',
 )
-ax6.legend(labels=['Recorded', 'Predicted'],frameon=True, loc='upper center')
+ax5.legend(labels=['Recorded', 'Predicted'],frameon=True, loc='upper center')
 
 fig.suptitle(
     'Predicted temperatue and wind induced indoor/outdoor pressure difference at the Indianapolis site',
@@ -166,18 +160,18 @@ plt.savefig('../../figures/transport_implications/pressure_prediction.pdf')
 fig, ax = plt.subplots()
 
 sns.boxplot(
-    data=df[['IndoorOutdoorPressure','dP_T', 'dP_T_wind', 'dP_T_wind_corr']],
+    data=df[['IndoorOutdoorPressure','dP_T', 'dP_T_wind']],
     ax=ax,
 )
 
-print(df[['IndoorOutdoorPressure','dP_T', 'dP_T_wind', 'dP_T_wind_corr']].describe())
+print(df[['IndoorOutdoorPressure','dP_T', 'dP_T_wind',]].describe())
 
 ax.set(
     title='Predicted temperature and wind contribution to building pressurization vs. recorded data at the EPA duplex',
     ylabel='$p_\\mathrm{in} \\; \\mathrm{(Pa)}$',
 )
 
-ax.set_xticklabels(['Recorded', 'Temp. contr.', 'Temp + wind', 'Temp. + dir. corr. wind'])
+ax.set_xticklabels(['Recorded', 'Temp. contr.', 'Temp + wind',])
 plt.tight_layout()
 plt.savefig('../../figures/transport_implications/pressure_prediction_boxplot.pdf', dpi=300)
 
